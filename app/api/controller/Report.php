a8a799bc59062770e122edd7b54d593219d336d2
simplezac1995 <296390911@qq.com>
2020-12-16 21:55:53 +0800

    前端队伍展示数据更改为代理判断

--- app/api/controller/Report.php
+++ app/api/controller/Report.php
@@ -163,82 +163,136 @@ class Report extends BaseController{
             'team'=>0,
             'cash_count'=>0
         ];
-        //团队总余额
-        $sql = "select sum(balance) s from t_account a inner join t_user b on a.user_id=b.id where b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId}";
-        $res = Db::query($sql);
-        if($res && $res[0]['s']){
-            $data['balance'] = $res[0]['s'];
+
+        $is_agent = Db::name("user")->where("id", $this->userId)->value('is_agent');
+        if($is_agent == 1){//代理展示数据
+            $ids = $id = $this->userId;
+            $line = [];//无限级下级
+            // $line[] = $id;
+            while ($ids) {
+                $users = Db::name("user")->field('id')->where("top1","in",$ids)->select()->toArray();
+                if(empty($users)){
+                    break;
+                }
+                $ids   = array_column($users, 'id');
+                $line = array_merge($data, $ids);
+            }
+
+            $teamBalanceAgent = Db::name("account")->where("user_id", "in", $line)->sum("balance");//团队总余额
+            $rechargeTotalAgent = Db::name("recharge")->where(['status'=>1])->where("user_id", "in", $line)->sum("money");//团队总充值
+            $withdrawTotalAgent = Db::name("cash")->where(['status'=>1])->where("user_id", "in", $line)->sum("money");//团队总提现
+            $teamCountAgent = count($line);
+
+            $data['balance'] = $teamBalanceAgent;
+            $data['recharge'] = $rechargeTotalAgent;
+            $data['cash'] = $withdrawTotalAgent;
+            $data['team'] = $teamCountAgent;
+
+            $res = Db::query("select count(*) s from t_user where top1=:user_id", ['user_id' =>$this->userId]);//直推人数
+            if($res){
+                $data['tj1'] = $res[0]['s'];
+            }
+            
+            $vips = get_vipcards();
+            $vipid = array_key_first($vips);
+            $res = Db::query("select count(*) s from (select id from t_user where vipcard_id>'{$vipid}' and top1={$this->userId} group by id) c");
+            if($res && $res[0]['s']){
+                $data['tx1'] = $res[0]['s'];
+            }
+            
+            $res = Db::query("select count(*) s from (select id from t_user where vipcard_id>'{$vipid}' and top2={$this->userId} group by id) c");
+            if($res && $res[0]['s']){
+                $data['tx2'] = $res[0]['s'];
+            }
+
+            $res = Db::query("select count(*) s from (select id from t_user where vipcard_id>'{$vipid}' and top3={$this->userId} group by id) c");
+            if($res && $res[0]['s']){
+                $data['tx3'] = $res[0]['s'];
+            }
+            //提现人数
+            $res = Db::query("select count(*) s from t_cash a inner join t_user b on a.user_id=b.id where a.status=1 and (b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId})");
+            if($res){
+                $data['cash_count'] = $res[0]['s'];
+            }
+            
+            $data['deposit'] = $data['tx1']+$data['tx2']+$data['tx3'];
+        }else{
+            //团队总余额
+            $sql = "select sum(balance) s from t_account a inner join t_user b on a.user_id=b.id where b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId}";
+            $res = Db::query($sql);
+            if($res && $res[0]['s']){
+                $data['balance'] = $res[0]['s'];
+            }
+            
+            //团队总充值
+            $res = Db::query("select sum(money) s from t_recharge a inner join t_user b on a.user_id=b.id where a.status=1 and (b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId})");
+            if($res && $res[0]['s']){
+                $data['recharge'] = $res[0]['s'];
+            }
+            
+            //团队总提现
+            $res = Db::query("select sum(money) s from t_cash a inner join t_user b on a.user_id=b.id where a.status=1 and (b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId})");
+            if($res && $res[0]['s']){
+                $data['cash'] = $res[0]['s'];
+            }
+            
+            //存款人数  先查初级会员VIPID  只要大于这个VIP的，说明是有充值过的
+            $vips = get_vipcards();
+            $vipid = array_key_first($vips);
+    //         $sql = "select count(*) s from (select a.user_id s from t_recharge a inner join t_user b on a.user_id=b.id where b.vipcard_id>'{$vipid}'  and a.status=1 and (b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId}) group by a.user_id) c";
+            
+    //         $res = Db::query($sql);
+    //         if($res && $res[0]['s']){
+    //             $data['deposit'] = $res[0]['s'];
+    //         }
+            
+            //直推人数 一级
+            $res = Db::query("select count(*) s from t_user where top1=:user_id", ['user_id' =>$this->userId]);
+            if($res){
+                $data['tj1'] = $res[0]['s'];
+            }
+            
+            $res = Db::query("select count(*) s from (select id from t_user where vipcard_id>'{$vipid}' and top1={$this->userId} group by id) c");
+            if($res && $res[0]['s']){
+                $data['tx1'] = $res[0]['s'];
+            }
+            
+            //二级推荐人数
+            $res = Db::query("select count(*) s from t_user where top2=:user_id", ['user_id' =>$this->userId]);
+            if($res){
+                $data['tj2'] = $res[0]['s'];
+            }
+            
+            $res = Db::query("select count(*) s from (select id from t_user where vipcard_id>'{$vipid}' and top2={$this->userId} group by id) c");
+            if($res && $res[0]['s']){
+                $data['tx2'] = $res[0]['s'];
+            }
+            
+            //三级推荐人数
+            $res = Db::query("select count(*) s from t_user where top3=:user_id", ['user_id' =>$this->userId]);
+            if($res){
+                $data['tj3'] = $res[0]['s'];
+            }
+            $res = Db::query("select count(*) s from (select id from t_user where vipcard_id>'{$vipid}' and top3={$this->userId} group by id) c");
+            if($res && $res[0]['s']){
+                $data['tx3'] = $res[0]['s'];
+            }
+            
+            //团队人数
+            $data['team'] = $data['tj1']+$data['tj2']+$data['tj3'];
+    //         $res = Db::query("select count(*) s t_user where top1=:user_id and (b.top1=:user_id or b.top2=:user_id or b.top3=:user_id)", ['user_id' =>$this->userId]);
+    //         if($res){
+    //             $data['team'] = $res[0]['s'];
+    //         }
+            
+            //提现人数
+            $res = Db::query("select count(*) s from t_cash a inner join t_user b on a.user_id=b.id where a.status=1 and (b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId})");
+            if($res){
+                $data['cash_count'] = $res[0]['s'];
+            }
+            
+            $data['deposit'] = $data['tx1']+$data['tx2']+$data['tx3'];
         }
-        
-        //团队总充值
-        $res = Db::query("select sum(money) s from t_recharge a inner join t_user b on a.user_id=b.id where a.status=1 and (b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId})");
-        if($res && $res[0]['s']){
-            $data['recharge'] = $res[0]['s'];
-        }
-        
-        //团队总提现
-        $res = Db::query("select sum(money) s from t_cash a inner join t_user b on a.user_id=b.id where a.status=1 and (b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId})");
-        if($res && $res[0]['s']){
-            $data['cash'] = $res[0]['s'];
-        }
-        
-        //存款人数  先查初级会员VIPID  只要大于这个VIP的，说明是有充值过的
-        $vips = get_vipcards();
-        $vipid = array_key_first($vips);
-//         $sql = "select count(*) s from (select a.user_id s from t_recharge a inner join t_user b on a.user_id=b.id where b.vipcard_id>'{$vipid}'  and a.status=1 and (b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId}) group by a.user_id) c";
-        
-//         $res = Db::query($sql);
-//         if($res && $res[0]['s']){
-//             $data['deposit'] = $res[0]['s'];
-//         }
-        
-        //直推人数 一级
-        $res = Db::query("select count(*) s from t_user where top1=:user_id", ['user_id' =>$this->userId]);
-        if($res){
-            $data['tj1'] = $res[0]['s'];
-        }
-        
-        $res = Db::query("select count(*) s from (select id from t_user where vipcard_id>'{$vipid}' and top1={$this->userId} group by id) c");
-        if($res && $res[0]['s']){
-            $data['tx1'] = $res[0]['s'];
-        }
-        
-        //二级推荐人数
-        $res = Db::query("select count(*) s from t_user where top2=:user_id", ['user_id' =>$this->userId]);
-        if($res){
-            $data['tj2'] = $res[0]['s'];
-        }
-        
-        $res = Db::query("select count(*) s from (select id from t_user where vipcard_id>'{$vipid}' and top2={$this->userId} group by id) c");
-        if($res && $res[0]['s']){
-            $data['tx2'] = $res[0]['s'];
-        }
-        
-        //三级推荐人数
-        $res = Db::query("select count(*) s from t_user where top3=:user_id", ['user_id' =>$this->userId]);
-        if($res){
-            $data['tj3'] = $res[0]['s'];
-        }
-        $res = Db::query("select count(*) s from (select id from t_user where vipcard_id>'{$vipid}' and top3={$this->userId} group by id) c");
-        if($res && $res[0]['s']){
-            $data['tx3'] = $res[0]['s'];
-        }
-        
-        //团队人数
-        $data['team'] = $data['tj1']+$data['tj2']+$data['tj3'];
-//         $res = Db::query("select count(*) s t_user where top1=:user_id and (b.top1=:user_id or b.top2=:user_id or b.top3=:user_id)", ['user_id' =>$this->userId]);
-//         if($res){
-//             $data['team'] = $res[0]['s'];
-//         }
-        
-        //提现人数
-        $res = Db::query("select count(*) s from t_cash a inner join t_user b on a.user_id=b.id where a.status=1 and (b.top1={$this->userId} or b.top2={$this->userId} or b.top3={$this->userId})");
-        if($res){
-            $data['cash_count'] = $res[0]['s'];
-        }
-        
-        $data['deposit'] = $data['tx1']+$data['tx2']+$data['tx3'];
-        
         return action_succ($data);
     }
     
