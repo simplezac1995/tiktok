c6d0b14f6979dbbf6ea09d2b728e94b41c5df388
simplezac1995 <296390911@qq.com>
2020-12-21 14:55:06 +0800

    登录修正

--- app/admin/controller/Login.php
+++ app/admin/controller/Login.php
@@ -57,7 +57,8 @@ class Login
                 $powers = Db::name("role")->where("id",$userInfo['role_id'])->value("powers");
             }
             
-            $ip = $_SERVER['HTTP_X_REAL_IP'];
+            // $ip = $_SERVER['HTTP_X_REAL_IP'];
+            $ip = $this->request->ip();
             Db::name("admin_user")->where("id",$userInfo['id'])->update([
                 'powers'=>$powers,
                 'last_login'=>time(),
